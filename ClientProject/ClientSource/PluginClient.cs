b6231b287f7a3f0c3c5e6ab617894c9cae1762fc
zhurengong <2731412072@qq.com>
2026-05-04 00:08:22 +0800

    init

---
+++ ClientProject/ClientSource/PluginClient.cs
@@ -0,0 +1,85 @@
+﻿using HarmonyLib;
+using System.Diagnostics;
+
+namespace PerfEnhancedSubEditor;
+
+public partial class Plugin
+{
+    public static int ParallelismLevel => Math.Min(Environment.ProcessorCount, 4);
+    private static Stopwatch performanceTimer = new();
+    private static AttachedProperty<bool> isEntityCulled = AttachedProperty<bool>.Create(false);
+
+    [HarmonyPatch(typeof(SubEditorScreen), nameof(SubEditorScreen.Draw)), HarmonyPrefix]
+    private static void SubEditorScreen_Draw_Prefix()
+    {
+        performanceTimer.Restart();
+
+        var entities = MapEntity.MapEntityList;
+        Rectangle camView = (Screen.Selected as SubEditorScreen)!.Cam.WorldView;
+
+        if (entities.Count > 0)
+        {
+            Partitioner.Create(0, entities.Count, 256)
+                .AsParallel()
+                .WithDegreeOfParallelism(ParallelismLevel)
+                .WithMergeOptions(ParallelMergeOptions.AutoBuffered)
+                .ForAll(range =>
+                {
+                    for (int i = range.Item1; i < range.Item2; i++)
+                    {
+                        MapEntity e = entities[i];
+                        isEntityCulled.SetValue(e, !e.IsVisible(camView));
+                    }
+                });
+        }
+
+        performanceTimer.Stop();
+
+        GameMain.PerformanceCounter.AddElapsedTicks("Draw:PerfEnhancedSubEditor", performanceTimer.ElapsedTicks);
+    }
+
+    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawBack)), HarmonyPrefix]
+    static void Submarine_DrawBack_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
+        => InjectRenderCulling(ref predicate, editing);
+
+    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawPaintedColors)), HarmonyPrefix]
+    static void Submarine_DrawPaintedColors_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
+        => InjectRenderCulling(ref predicate, editing);
+
+    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawDamageable)), HarmonyPrefix]
+    static void Submarine_DrawDamageable_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
+        => InjectRenderCulling(ref predicate, editing);
+
+    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawFront)), HarmonyPrefix]
+    static void Submarine_DrawFront_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
+        => InjectRenderCulling(ref predicate, editing);
+
+    static void InjectRenderCulling(ref Predicate<MapEntity>? predicate, bool editing)
+    {
+        if (editing && SubEditorScreen.IsSubEditor())
+        {
+            var originalPredicate = predicate;
+
+            predicate = originalPredicate == null
+                 ? entity => !isEntityCulled.GetValue(entity)
+                 : entity => !isEntityCulled.GetValue(entity) && originalPredicate(entity);
+        }
+    }
+
+    [HarmonyPatch(typeof(SubEditorScreen), nameof(SubEditorScreen.AddToGUIUpdateList)), HarmonyPrefix]
+    private static void SubEditorScreen_AddToGUIUpdateList_Prefix(SubEditorScreen __instance)
+    {
+        __instance.EntityMenu.Visible = __instance.entityMenuOpenState > 0.0f;
+    }
+
+    [HarmonyPatch(typeof(SubEditorScreen), nameof(SubEditorScreen.AddToGUIUpdateList)), HarmonyPostfix]
+    private static void SubEditorScreen_AddToGUIUpdateList_Postfix(SubEditorScreen __instance)
+    {
+        if (!__instance.EntityMenu.Visible)
+        {
+            __instance.EntityMenu.Visible = true;
+            __instance.EntityMenu.AddToGUIUpdateList(ignoreChildren: true);
+            __instance.ToggleEntityMenuButton.AddToGUIUpdateList();
+        }
+    }
+}
---
+++ SharedProject/SharedSource/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+
+namespace PerfEnhancedSubEditor;
+
+[HarmonyPatch]
+public partial class Plugin : IAssemblyPlugin
+{
+    // These are automatically assigned by the plugin service after the Constructor is called
+    #pragma warning disable CS8618
+    public IConfigService ConfigService { get; set; }
+    public IPluginManagementService PluginManagementService { get; set; }
+    public ILoggerService LoggerService { get; set; }
+    public IConsoleCommandsService ConsoleCommandsService { get; set; }
+    #pragma warning restore CS8618
+
+    public ContentPackage _package = null!;
+
+    public Harmony? harmony;
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public void Initialize()
+    {
+        // When your plugin is loading, use this instead of the constructor for code relying on
+        // the services above.
+
+        if (!PluginManagementService.TryGetPackageForPlugin<Plugin>(out _package))
+        {
+            LoggerService.LogError("Failed to find package!");
+            return;
+        }
+
+        harmony = new("PerfEnhancedSubEditor");
+        harmony.PatchAll();
+    }
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public void OnLoadCompleted()
+    {
+        // After all plugins have loaded
+        // Put code that interacts with other plugins here.
+    }
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public void PreInitPatching()
+    {
+        // Called right after the constructor
+    }
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public void Dispose()
+    {
+        // Cleanup your plugin!
+
+        harmony?.UnpatchSelf();
+        harmony = null;
+    }
+
+}
