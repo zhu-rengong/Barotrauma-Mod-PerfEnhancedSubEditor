4459fae1620344ecce5f54954aaad9b6d8d19575
zhurengong <2731412072@qq.com>
2026-05-05 20:59:15 +0800

    1.0.3 - Addressed a performance issue where increasing the Hull's size caused a drastic frame rate drop during editing. Meanwhile this will also improve the speed of loading submarines in the editor. - Skip drawing painted colors of hulls (reduces average performance consumption by 25%)

--- ClientProject/ClientSource/PluginClient.cs
+++ ClientProject/ClientSource/PluginClient.cs
@@ -5,6 +5,8 @@ namespace PerfEnhancedSubEditor;
 
 public partial class Plugin
 {
+    #region Frustum Culling
+
     public static int ParallelismLevel => Math.Min(Environment.ProcessorCount, 4);
     private static Stopwatch performanceTimer = new();
     private static AttachedProperty<bool> isEntityCulled = AttachedProperty<bool>.Create(false);
@@ -22,7 +24,6 @@ public partial class Plugin
             Partitioner.Create(0, entities.Count, 256)
                 .AsParallel()
                 .WithDegreeOfParallelism(ParallelismLevel)
-                .WithMergeOptions(ParallelMergeOptions.AutoBuffered)
                 .ForAll(range =>
                 {
                     for (int i = range.Item1; i < range.Item2; i++)
@@ -40,10 +41,6 @@ public partial class Plugin
 
     [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawBack)), HarmonyPrefix]
     static void Submarine_DrawBack_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
-        => InjectRenderCulling(ref predicate, editing);
-
-    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawPaintedColors)), HarmonyPrefix]
-    static void Submarine_DrawPaintedColors_Prefix(ref Predicate<MapEntity>? predicate, bool editing)
         => InjectRenderCulling(ref predicate, editing);
 
     [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawDamageable)), HarmonyPrefix]
@@ -66,6 +63,10 @@ public partial class Plugin
         }
     }
 
+    #endregion
+
+    #region Hide Entity Menu
+
     [HarmonyPatch(typeof(SubEditorScreen), nameof(SubEditorScreen.AddToGUIUpdateList)), HarmonyPrefix]
     private static void SubEditorScreen_AddToGUIUpdateList_Prefix(SubEditorScreen __instance)
     {
@@ -82,4 +83,84 @@ public partial class Plugin
             __instance.ToggleEntityMenuButton.AddToGUIUpdateList();
         }
     }
+
+    #endregion
+
+    #region Performance fix for resizing hull
+
+    private static bool isResizingHull;
+
+    [HarmonyPatch(typeof(MapEntity), nameof(MapEntity.Resizing), MethodType.Setter), HarmonyPrefix]
+    static void MapEntity_Resizing_Setter_Prefix(ref bool value)
+    {
+        if (!isResizingHull) { return; }
+
+        if (!value)
+        {
+            Item.UpdateHulls();
+            Gap.UpdateHulls();
+        }
+    }
+
+    [HarmonyPatch(typeof(MapEntity), nameof(MapEntity.UpdateResizing)), HarmonyPrefix]
+    static void MapEntity_UpdateResizing_Prefix(MapEntity __instance)
+    {
+        isResizingHull = __instance is Hull;
+    }
+
+    [HarmonyPatch(typeof(MapEntity), nameof(MapEntity.UpdateResizing)), HarmonyFinalizer]
+    static void MapEntity_UpdateResizing_Finalizer(MapEntity __instance)
+    {
+        isResizingHull = false;
+    }
+
+    [HarmonyPatch(typeof(Hull), nameof(Hull.Rect), MethodType.Setter), HarmonyPrefix]
+    static bool Hull_Rect_Setter_Prefix(Hull __instance, ref Rectangle value)
+    {
+        if (SubEditorScreen.IsSubEditor())
+        {
+            float prevOxygenPercentage = __instance.OxygenPercentage;
+            __instance.rect = value;
+            __instance.OxygenPercentage = prevOxygenPercentage;
+            return false;
+        }
+
+        return true;
+    }
+
+    [HarmonyPatch(typeof(Hull), nameof(Hull.Load)), HarmonyPrefix]
+    static void Hull_Load_Prefix(Hull __instance, ContentXElement element)
+    {
+        if (SubEditorScreen.IsSubEditor())
+        {
+            element.GetAttribute("backgroundsections")?.Remove();
+        }
+    }
+
+    [HarmonyPatch(typeof(Hull), nameof(Hull.CreateBackgroundSections)), HarmonyPrefix]
+    static bool Hull_CreateBackgroundSections_Prefix(Hull __instance)
+    {
+        return !SubEditorScreen.IsSubEditor();
+    }
+
+    [HarmonyPatch(typeof(Hull), nameof(Hull.RefreshAveragePaintedColor)), HarmonyPrefix]
+    static bool Hull_RefreshAveragePaintedColor_Prefix(Hull __instance)
+    {
+        return !SubEditorScreen.IsSubEditor();
+    }
+
+    private static bool isHullBackgroundSectionsDefined;
+
+    [HarmonyPatch(typeof(Submarine), nameof(Submarine.DrawPaintedColors)), HarmonyPrefix]
+    static bool Submarine_DrawPaintedColors_Prefix(bool editing)
+    {
+        if (editing && SubEditorScreen.IsSubEditor())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    #endregion
 }
--- SharedProject/SharedSource/Plugin.cs
+++ SharedProject/SharedSource/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System.Diagnostics;
 
 namespace PerfEnhancedSubEditor;
 
@@ -6,12 +7,12 @@ namespace PerfEnhancedSubEditor;
 public partial class Plugin : IAssemblyPlugin
 {
     // These are automatically assigned by the plugin service after the Constructor is called
-    #pragma warning disable CS8618
+#pragma warning disable CS8618
     public IConfigService ConfigService { get; set; }
     public IPluginManagementService PluginManagementService { get; set; }
     public ILoggerService LoggerService { get; set; }
     public IConsoleCommandsService ConsoleCommandsService { get; set; }
-    #pragma warning restore CS8618
+#pragma warning restore CS8618
 
     public ContentPackage _package = null!;
 
@@ -31,6 +32,29 @@ public partial class Plugin : IAssemblyPlugin
 
         harmony = new("PerfEnhancedSubEditor");
         harmony.PatchAll();
+
+        var currentAssembly = Assembly.GetExecutingAssembly();
+
+        foreach (var method in harmony.GetPatchedMethods())
+        {
+            var patchInfo = Harmony.GetPatchInfo(method);
+
+            foreach (var prefix in patchInfo.Prefixes)
+            {
+                if (prefix.PatchMethod.DeclaringType is Type type && type.Assembly == currentAssembly)
+                {
+                    LoggerService.LogWarning($" A prefix method {type.FullName} {prefix.PatchMethod} has been patched onto method {method.DeclaringType?.FullName} {method}");
+                }
+            }
+
+            foreach (var postfix in patchInfo.Postfixes)
+            {
+                if (postfix.PatchMethod.DeclaringType is Type type && type.Assembly == currentAssembly)
+                {
+                    LoggerService.LogWarning($" A postfix method {type.FullName} {postfix.PatchMethod} has been patched onto method {method.DeclaringType?.FullName} {method}");
+                }
+            }
+        }
     }
 
     [MethodImpl(MethodImplOptions.NoOptimization)]
